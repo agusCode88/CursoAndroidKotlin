d599f9ff8e73d9dd7b061559b5567053ca6c149d
agusCode88 <agus.romero.salazar@gmail.com>
2023-04-23 22:32:41 -0400

    Condicionales y When
    Se hacen ejecicios de ejemplo hasta el when , se abordan todos sus usos

--- Condicionales.kt
+++ Condicionales.kt
@@ -1,20 +1,24 @@
 package com.example.aprendiendokotlin
 
-fun main () {
+fun main() {
 
-    ifBasicoSinParametros()
-    var esVerdad = ifBasicoConParametros("alberto")
-    if(esVerdad){
-        println("Es cierto lo que dices, los dos se llaman igual")
-    } else {
-        println("No es verdad lo que dices, no se llaman igual")
-    }
-    var esVerdad2 = ifAnidados("Claudio","15")
-    if (esVerdad2 ){
-        println("La persona está habilitada para poder sufragar");
-    }else {
-        println("La persona esta inhabilitada para poder sufragar")
-    }
+//    ifBasicoSinParametros()
+//    var esVerdad = ifBasicoConParametros("alberto")
+//    if (esVerdad) {
+//        println("Es cierto lo que dices, los dos se llaman igual")
+//    } else {
+//        println("No es verdad lo que dices, no se llaman igual")
+//    }
+//    var esVerdad2 = ifAnidados("Claudio", "15")
+//    if (esVerdad2) {
+//        println("La persona está habilitada para poder sufragar");
+//    } else {
+//        println("La persona esta inhabilitada para poder sufragar")
+//    }
+//    operacionesConIfElse(4)
+//    getMesAnioconWhen(13)
+
+    trabajandoVariableAny(true)
 
 }
 
@@ -41,48 +45,140 @@ fun ifBasicoSinParametros() {
     }
 }
 
-fun ifBasicoConParametros(nombreParametro : String) : Boolean {
+fun ifBasicoConParametros(nombreParametro: String): Boolean {
 
-        var nombre : String = "Alberto"
-        var nombrePrueba: String = "alberto"
-        var esVerdad = true
+    var nombre: String = "Alberto"
+    var nombrePrueba: String = "alberto"
+    var esVerdad = true
 
-        if (nombre == nombreParametro){
+    if (nombre == nombreParametro) {
 
-            esVerdad = true
+        esVerdad = true
 
-        } else {
+    } else {
 
-            esVerdad = false
-        }
-        if(nombre.equals(nombreParametro)){
+        esVerdad = false
+    }
+    if (nombre.equals(nombreParametro)) {
 
-            esVerdad = true
-            return esVerdad
+        esVerdad = true
+        return esVerdad
 
-        }else {
-            esVerdad = false
-            return esVerdad
-        }
+    } else {
+        esVerdad = false
+        return esVerdad
+    }
 }
 
-fun ifAnidados(nombreParametro: String, edadParametro: String ): Boolean{
+fun ifAnidados(nombreParametro: String, edadParametro: String): Boolean {
 
     var nombrePersona = "Claudio"
-    val mayoriaEdad:Int = 18
+    val mayoriaEdad: Int = 18
     var habilitadoMayorEdad = true
+    var quiereVotar = true
 
-   if(nombrePersona.equals(nombreParametro)){
+    if (nombrePersona.equals(nombreParametro)) {
 
-       if (edadParametro.toInt() < mayoriaEdad ){
-           habilitadoMayorEdad = false;
-       }
-       if(edadParametro.toInt()>= mayoriaEdad){
-           habilitadoMayorEdad = true
-       }
-   } else {
-           habilitadoMayorEdad = false
-   }
+        if (edadParametro.toInt() < mayoriaEdad) {
+            habilitadoMayorEdad = false;
+            quiereVotar = false
+        }
+        if (edadParametro.toInt() >= mayoriaEdad && quiereVotar) {
+            habilitadoMayorEdad = true
+        }
 
-    return  habilitadoMayorEdad
+    } else {
+        habilitadoMayorEdad = false
+    }
+
+    return habilitadoMayorEdad
+}
+
+fun operacionesConIfElse(mesAnio: Int) {
+
+    if (mesAnio == 1) {
+        println("Es el mes de enero")
+    } else if (mesAnio == 2) {
+        println("Es el mes de febrero")
+    } else if (mesAnio == 3) {
+        println("Es el mes de marzo")
+    } else if (mesAnio == 4) {
+        println("Es el mes de abril")
+    } else if (mesAnio == 5) {
+        println("Es el mes de mayo")
+    } else if (mesAnio == 6) {
+        println("Es el mes de junio")
+    } else if (mesAnio == 7) {
+        println("Es el mes de julio")
+    } else if (mesAnio == 8) {
+        println("Es el mes de agosto")
+    } else if (mesAnio == 9) {
+        println("Es el mes de septiembre")
+    } else if (mesAnio == 10) {
+        println("Es el mes de octubre")
+    } else if (mesAnio == 11) {
+        println("Es el mes de noviembre")
+    } else if (mesAnio == 12) {
+        println("Es el mes de diciembre")
+    } else {
+        println("No es un mes del año")
+    }
+}
+
+fun getMesAnioconWhen(mesAnio: Int) {
+
+    when (mesAnio) {
+        1 -> println("Es enero")
+        2 -> println("Es febrero")
+        3 -> println("Es marzo")
+        4 -> println("Es abril")
+        5 -> println("Es mayo")
+        6 -> println("Es junio")
+        7 -> println("Es julio")
+        8 -> println("Es agosto")
+        9 -> println("Es septiembre")
+        10 -> println("Es octubre")
+        11 -> println("Es noviembre")
+        12 -> println("Es diembre")
+        13 -> {
+            if (mesAnio == 13) {
+                println("Esta es una funcion secreta ")
+            }
+        }
+        else -> println("No es ningun mes")
+
+    }
+
+}
+
+fun getTrimesteAnio(valorTrimeste: Int) {
+
+    when (valorTrimeste) {
+
+        1, 2, 3 -> println("primer trimestre")
+        4, 5, 6 -> println("segundo trimestre")
+        7, 8, 9 -> println("tercer trimestre")
+        10, 11, 12 -> println("cuarto trimestre")
+    }
+
+}
+
+fun getSemestreAnio(valorTrimeste: Int) {
+
+    when (valorTrimeste) {
+        in 1..6 -> println("Primer semestre del año")
+        in 7..12 -> println("Segundo semestre del año")
+        !in 7..12-> println("Semestre no válido")
+    }
+
+}
+
+fun trabajandoVariableAny(valor: Any){
+
+    when (valor){
+        is Int -> valor + valor
+        is String-> valor + "Concatenado conmigo"
+        is Boolean-> if (valor) println("La variable es verdadera")
+    }
+
 }
