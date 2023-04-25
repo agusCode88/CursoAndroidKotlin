b9b6c9d4a17888041a4622d21b1f51923f7621f8
StivenFonseca <stiven.jfonseca@gmail.com>
2023-04-24 22:01:17 -0400

    segunda clase de Kotlin

---
+++ PrecioCine.kt
@@ -0,0 +1,39 @@
+package com.example.aprendiendokotlin
+
+//Las entradas de cine suelen tener un precio diferente según la edad de los espectadores.
+//
+//En el código inicial que se proporciona en el siguiente fragmento de código, escribe un programa que calcule los precios de estas entradas basados en la edad:
+//
+//Un precio de entrada infantil de USD 15 para personas de 12 años o menos.
+//Un precio de entrada estándar de USD 30 para personas de entre 13 y 60 años. Los lunes, un precio estándar con descuento de USD 25 para el mismo grupo etario
+//Un precio para adultos mayores de USD 20 para personas de 61 años o más (asumimos que la edad máxima de un espectador es de 100 años)
+//Un valor de -1 para indicar que el precio no es válido cuando un usuario ingresa una edad fuera de las especificaciones
+
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    // Write your code below
+    if (age <= 12) {
+        return 15
+    } else if (age in 13..60) {
+        if (isMonday) {
+            return 25
+        } else {
+            return 30
+        }
+    } else if (age in 61..100) {
+        return 20
+    } else {
+        return -1
+    }
+}
