7d72b5b73552e595f8ba1d53ed6ccc19256a5166
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-04-25 18:12:31 -0400

    avances

---
+++ barbara-temperatura.kt
@@ -0,0 +1,42 @@
+package com.example.aprendiendokotlin
+
+/*
+Conversor de temperatura
+En el mundo, se usan tres escalas de temperatura principales: Celsius, Fahrenheit y Kelvin.
+
+En el código inicial que se proporciona en el siguiente fragmento de código,
+escribe un programa que convierta una temperatura de una escala a otra con estas fórmulas:
+
+De grados Celsius a Fahrenheit: °F = 9/5 (°C) + 32
+Kelvin a Celsius: °C = K - 273.15
+De Fahrenheit a Kelvin: K = 5/9 (°F - 32) + 273.15
+Ten en cuenta que el método String.format("%.2f", /* measurement */ ) se usa para convertir un número en un tipo String con 2 decimales.
+* */
+fun main() {
+    // Ejemplos de uso
+    printFinalTemperature(25.0, "Celsius", "Fahrenheit") { celsiusToFahrenheit(it) }
+    printFinalTemperature(300.0, "Kelvin", "Celsius") { kelvinToCelsius(it) }
+    printFinalTemperature(98.6, "Fahrenheit", "Kelvin") { fahrenheitToKelvin(it) }
+}
+
+fun celsiusToFahrenheit(celsius: Double): Double {
+    return 9/5 * celsius + 32
+}
+
+fun kelvinToCelsius(kelvin: Double): Double {
+    return kelvin - 273.15
+}
+
+fun fahrenheitToKelvin(fahrenheit: Double): Double {
+    return 5/9 * (fahrenheit - 32) + 273.15
+}
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // dos decimales
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
