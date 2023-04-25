c2560c0d23f8dd9978456e0e39c4aa3be7395841
MagaLop <consuelomagana90@gmail.com>
2023-04-24 21:51:37 -0400

    commit ejercicio1 - primer intento

---
+++ Clase2/Ejercicio1.kt
@@ -0,0 +1,37 @@
+package com.example.aprendiendokotlin.Clase2
+/*
+----------
+Por lo general, el teléfono te proporciona un resumen de las notificaciones.
+
+En el código inicial que se proporciona en el siguiente fragmento de código,
+escribe un programa que imprima el mensaje de resumen según la cantidad de notificaciones que recibiste.
+El mensaje debe incluir lo siguiente:
+
+La cantidad exacta de notificaciones cuando haya menos de 100
+99+ como cantidad de notificaciones cuando haya 100 o más
+---------
+(el codigo estuvo aqui)
+---------
+Completa la función printNotificationSummary() para que el programa imprima estas líneas:
+
+
+You have 51 notifications.
+Your phone is blowing up! You have 99+ notifications.
+ */
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    if(numberOfMessages < 51){
+        println("You have $numberOfMessages notifications.")
+    }else{
+        println("Your phone is blowing up! You have 99+ notifications.")
+    }
+}
+
