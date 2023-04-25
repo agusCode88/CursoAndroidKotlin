365738a4979f2e3b7553ce713aadfa4f83f406ed
UrzuaAlarcon <122647461+UrzuaAlarcon@users.noreply.github.com>
2023-04-25 18:48:22 -0400

    Practica conceptos basicos de Kotlin Conversor de temperatura

---
+++ ClaseKotlin2/ConversorTemperatura.kt
@@ -0,0 +1,46 @@
+package com.example.aprendiendokotlin.ClaseKotlin2
+
+/*Conversor de temperatura
+
+En el mundo, se usan tres escalas de temperatura principales: Celsius, Fahrenheit y Kelvin.
+En el código inicial que se proporciona en el siguiente fragmento de código, escribe un programa
+que convierta una temperatura de una escala a otra con estas fórmulas:
+
+De grados Celsius a Fahrenheit: °F = 9/5 (°C) + 32
+Kelvin a Celsius: °C = K - 273.15
+De Fahrenheit a Kelvin: K = 5/9 (°F - 32) + 273.15
+Ten en cuenta que el método String.format("%.2f", *//* measurement *//* ) se usa para convertir
+un número en un tipo String con 2 decimales.*/
+
+
+fun main() {
+
+    val celsiusAFahrenheit: (Double) -> Double = { celsius -> (9.0 / 5.0 * celsius) + 32 }
+    val kelvinACelsius: (Double) -> Double = { kelvin -> kelvin - 273.15 }
+    val fahrenheAToKelvin: (Double) -> Double = { fahrenheit -> 5.0 / 9.0 * (fahrenheit - 32) + 273.15 }
+
+    printFinalTemperature(27.0,"Celsius","Fahrenheit",celsiusAFahrenheit)
+    printFinalTemperature(350.0, "Kelvin", "Celsius", kelvinACelsius)
+    printFinalTemperature(10.0,"Fahrenheit", "Kelvin", fahrenheAToKelvin)
+
+}
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
+/*
+Completa la función main() para que llame a la función printFinalTemperature() e imprima las siguientes
+líneas. Debes pasar argumentos para la fórmula de conversión y temperatura.
+Sugerencia: Te recomendamos usar valores Double para evitar el truncamiento de Integer durante las
+operaciones de división.
+
+27.0 degrees Celsius is 80.60 degrees Fahrenheit.
+350.0 degrees Kelvin is 76.85 degrees Celsius.
+10.0 degrees Fahrenheit is 260.93 degrees Kelvin.*/
