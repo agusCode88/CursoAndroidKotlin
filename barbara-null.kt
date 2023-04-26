ffb362efc2d541ac3794e5f8e49d97074d8b8906
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-04-25 20:25:34 -0400

    T_T

--- barbara-null.kt
+++ barbara-null.kt
@@ -3,11 +3,12 @@ package com.example.aprendiendokotlin
 // nuabilidad
 
 fun main(){
-
+    nulabilidadEnKotlin()
 }
 
 
 fun nulabilidadEnKotlin(){
     var nombrePersona: String? = null
+    println(nombrePersona?.length)
 
 }
