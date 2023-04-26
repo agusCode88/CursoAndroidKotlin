228fac3ba3ecbcce5bb06ed5d38f421998dea7a8
StivenFonseca <stiven.jfonseca@gmail.com>
2023-04-26 18:37:44 -0400

    segunda clase de Kotlins

---
+++ bucles.kt
@@ -0,0 +1,8 @@
+package com.example.aprendiendokotlin
+
+fun practicaDeBucleFor() {
+    val listaDeFrutas = listOf("Manzana", "Pera", "Fresa", "Uva", "Piña")
+    for (fruta in listaDeFrutas) {
+        println(fruta)
+    }
+}
