8d84e7237c4aaff968c9b4be858b7dea527fbede
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-04-26 23:56:02 -0400

    codigo mejorado

--- Tarea4.kt
+++ Tarea4.kt
@@ -1,33 +1,17 @@
 package com.example.aprendiendokotlin
 
-/*
-Cree un programa en Kotlin que pregunte por pantalla la cantidad de elementos que tendrá el arreglo.
-Capture el dato de entrada y setee un arreglo de enteros del tamaño ingresado por el usuario.
-Una vez que haya creado el arreglo e indicado la cantidad de elementos ,proceda a llenar el arreglo
-con numeros por pantalla agregando cada elemento segun la cantidad que ingresó el usuario.
- Luego que haya llenado el arreglo ,muestre por pantalla un menu que indique las siguinetes opciones
- a realizar como usuario:
-
-1)Mostrar el contenido del arreglo en orden inverso.
-2)Imprimir el contenido y su indice del arreglo en orden normal.
-3)Mostrar el primer y el ultimo elemento del arreglo.
-4)Sumar los elementos del arreglo y mostrar el resultado por pantalla
-5) Actualizar o agregar un nuevo valor al arreglo y mostrar la nueva suma
-
-Considere hacer las validaciones correspondientes para que el programa no se caiga
-y acepte solo enteros como entrada por teclado.
- */
-
-
 fun main() {
-    elementos()
-    menu()
+
+    val arreglo = elementos()
+    menu(arreglo)
+
 
 }
 
-fun elementos() {
+fun elementos() : IntArray {
 
-    var cantidad: Int? = null
+    var cantidad: Int?
+    var arreglo = intArrayOf()
 
     do {
 
@@ -37,71 +21,82 @@ fun elementos() {
             cantidad = readln().toInt()
             if (cantidad == null) throw Exception()
 
-            var arreglo : IntArray = IntArray(cantidad)
-            var tamaño = arreglo
+            arreglo = IntArray(cantidad)
 
             for(i in arreglo.indices){
-
                 print("Ingrese un numero para la lista --> ")
-                var numero : Int = readln().toInt()
-                arreglo[i] = numero
+                arreglo[i] = readLine()?.toInt() ?: 0
             }
 
         } catch (e: Exception) {
             println("Ingrese un número válido.")
+            cantidad = null
         }
 
     }while (cantidad == null)
+
+    return arreglo
 }
 
-fun menu(){
+fun menu(arreglo: IntArray) {
+    var opc: Int
 
-    println("\nMENU PRINCIPAL")
-    println("-------------------------")
-    println("\n1.-Mostrar lista orden normal")
-    println("2.-Mostrar lista orden inverso")
-    println("3.-Mostrar primer y ultimo elemento")
-    println("4.-Sumar los elementos de la lista y mostrar")
-    println("5.-Actualizar o agregar un elemento a la lista y mostrar la nueva suma")
-    println("6.-Salir")
-    println("\nELIGE TU OPCION [1-2-3-4-5-6] --> ")
-    var opc : Int = readln().toInt()
-    
+    do {
+        println("\nMENU PRINCIPAL")
+        println("-------------------------")
+        println("\n1.-Mostrar lista orden normal")
+        println("2.-Mostrar lista orden inverso")
+        println("3.-Mostrar primer y ultimo elemento")
+        println("4.-Sumar los elementos de la lista y mostrar")
+        println("5.-Actualizar o agregar un elemento a la lista y mostrar la nueva suma")
+        println("6.-Salir")
+        println("\nELIGE TU OPCION [1-2-3-4-5-6] --> ")
+        opc = readLine()?.toIntOrNull() ?: 0
+
+        when (opc) {
+
+            1 -> mostrarNormal(arreglo)
+            2 -> mostrarInverso(arreglo)
+            3 -> mostrarPrimerUltimoElemento(arreglo)
+            4 -> sumaElementos(arreglo)
+            5 -> {
+                println("Ingresa la posicion del elemento que desea modificar --> ")
+                val posicion = readLine()?.toIntOrNull() ?: 0
+                println("Ingrese el valor del elemento --> ")
+                val valor = readLine()?.toIntOrNull() ?: 0
+                arreglo[posicion] = valor
+                sumaElementos(arreglo)
+            }
+            6 -> println("Adios")
+            else -> println("Opcion no valida")
+        }
+    }while (opc != 6)
 }
 
 fun mostrarNormal(arreglo : IntArray){
+    //contentToString retorna una representacion en forma de String de los elementos del array como si fuera una Lista.
+   println("Lista orden normal --> ${arreglo.contentToString()}") //Muestra los elementos
 
-   println(arreglo.contentToString()) //Muestra los elementos
 
 }
 
 fun mostrarInverso(arreglo : IntArray){
 
     arreglo.reverse() //Invierte el orden
-    println(arreglo.contentToString())
+    println("Lista en orden invertido --> ${arreglo.contentToString()}")
 }
 
-fun mostrarPrimerElemento(arreglo : IntArray){
+fun mostrarPrimerUltimoElemento(arreglo : IntArray){
 
-    println(arreglo.first()) //Muestra el primer elemento
-    println(arreglo.last())  //Muestra el último elemento
+    println("El primer elemento es --> ${arreglo.first()}") //Muestra el primer elemento
+    println("El ultimo elemento es --> ${arreglo.last()}")  //Muestra el último elemento
 }
-
-fun mostrarUltimoElemento(arreglo : IntArray){
-
-    println(arreglo.last())  //Muestra el último elemento
-}
-
 fun sumaElementos(arreglo: IntArray){
 
-    println(arreglo.sum())  //Suma todos los elementos
+    println("La suma de todos los elementos es --> ${arreglo.sum()}")  //Suma todos los elementos
 }
 
-fun modificarElemento(arreglo : IntArray){
 
-    arreglo[3] = 5  //Modifica el ele
-    println(arreglo.sum())
-}
 
 
 
