c7f17a1c84682c4ded0d7793f889f580e4a80050
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-04-27 19:03:32 -0400

    mejora 2.0

--- Tarea4.kt
+++ Tarea4.kt
@@ -49,7 +49,9 @@ fun menu(arreglo: IntArray) {
         println("3.-Mostrar primer y ultimo elemento")
         println("4.-Sumar los elementos de la lista y mostrar")
         println("5.-Actualizar o agregar un elemento a la lista y mostrar la nueva suma")
-        println("6.-Salir")
+        println("6.-Eliminar todos los elementos")
+        println("7.-Eliminar elemento por indice")
+        println("8.-Salir")
         println("\nELIGE TU OPCION [1-2-3-4-5-6] --> ")
         opc = readLine()?.toIntOrNull() ?: 0
 
@@ -59,25 +61,18 @@ fun menu(arreglo: IntArray) {
             2 -> mostrarInverso(arreglo)
             3 -> mostrarPrimerUltimoElemento(arreglo)
             4 -> sumaElementos(arreglo)
-            5 -> {
-                println("Ingresa la posicion del elemento que desea modificar --> ")
-                val posicion = readLine()?.toIntOrNull() ?: 0
-                println("Ingrese el valor del elemento --> ")
-                val valor = readLine()?.toIntOrNull() ?: 0
-                arreglo[posicion] = valor
-                sumaElementos(arreglo)
-            }
-            6 -> println("Adios")
+            5 -> modificar(arreglo)
+            6 -> eliminarTodo(arreglo)
+            7 -> eliminarElemento(arreglo)
+            8 -> println("Adios")
             else -> println("Opcion no valida")
         }
-    }while (opc != 6)
+    }while (opc != 7)
 }
 
 fun mostrarNormal(arreglo : IntArray){
     //contentToString retorna una representacion en forma de String de los elementos del array como si fuera una Lista.
    println("Lista orden normal --> ${arreglo.contentToString()}") //Muestra los elementos
-
-
 }
 
 fun mostrarInverso(arreglo : IntArray){
@@ -96,6 +91,36 @@ fun sumaElementos(arreglo: IntArray){
     println("La suma de todos los elementos es --> ${arreglo.sum()}")  //Suma todos los elementos
 }
 
+fun modificar(arreglo: IntArray) {
+
+    println("Ingresa la posicion del elemento que desea modificar --> ")
+    val posicion = readLine()?.toIntOrNull() ?: 0
+    println("Ingrese el valor del elemento --> ")
+    val valor = readLine()?.toIntOrNull() ?: 0
+    arreglo[posicion] = valor
+    sumaElementos(arreglo)
+}
+
+fun eliminarElemento(arreglo: IntArray){
+
+    println("Ingresa la posicion del elemento que desea eliminar --> ")
+    val posicion = readLine()?.toIntOrNull() ?: 0
+
+    val nuevoArreglo = arreglo.toMutableList()
+    nuevoArreglo.removeAt(posicion)
+    val arregloFinal = nuevoArreglo.toIntArray()
+
+    println(nuevoArreglo)
+}
+
+fun eliminarTodo(arreglo: IntArray){
+
+    val backupArreglo : IntArray = arreglo
+
+    arreglo.fill(0)
+    println(arreglo.contentToString())
+
+}
 
 
 
