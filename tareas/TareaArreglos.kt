d1685ae733562d32fbebdde6075fdcf8d883cf25
JavFuentes <javier.antonio8128@gmail.com>
2023-04-27 03:44:22 -0400

    tarea terminada!

--- tareas/TareaArreglos.kt
+++ tareas/TareaArreglos.kt
@@ -21,19 +21,19 @@ y acepte solo enteros como entrada por teclado.
  */
 
 
+fun main(args: Array<String>) {
+    val tamanioArreglo = solicitarTamanioArreglo()
+    val arreglo = crearArreglo(tamanioArreglo)
 
-fun main() {
-    val tamañoArreglo = solicitarTamañoArreglo()
-    val arreglo = crearArreglo(tamañoArreglo)
-
-    //llenarArreglo(arreglo)
-    //mostrarArregloInverso(arreglo)
-    //mostrarArregloNormal(arreglo)
-    //mostrarSumaArreglo(arreglo)
+    llenarArreglo(arreglo)
+    mostrarArregloInverso(arreglo)
+    mostrarArregloNormal(arreglo)
+    mostrarPrimerYUltimoElemento(arreglo)
+    mostrarSumaArreglo(arreglo)
+    agregarUnNuevoElementoYSumarlo(arreglo)
 }
 
-
-fun solicitarTamañoArreglo(): Int {
+fun solicitarTamanioArreglo(): Int {
     var tamanio: Int? = null
     do {
         println("Ingrese la cantidad de elementos del arreglo:")
@@ -46,9 +46,82 @@ fun solicitarTamañoArreglo(): Int {
     } while (tamanio == null)
 
     return tamanio
-
 }
 
-fun crearArreglo(tamaño: Int): IntArray {
-    return IntArray(tamaño)
+fun crearArreglo(tamanio: Int): IntArray {
+    return IntArray(tamanio)
 }
+
+fun llenarArreglo(arreglo: IntArray) {
+    for (i in arreglo.indices) {
+        var valor: Int? = null
+        do {
+            println("Ingrese el elemento número ${i + 1}:")
+            try {
+                val input = readLine()
+                if (input != null && input.toIntOrNull() != null) {
+                    valor = input.toInt()
+                } else {
+                    throw NumberFormatException()
+                }
+            } catch (e: NumberFormatException) {
+                println("Ingrese un número válido.")
+            }
+        } while (valor == null)
+        arreglo[i] = valor
+    }
+}
+
+
+fun mostrarArregloInverso(arreglo: IntArray) {
+    val arregloInverso = arreglo.copyOf().apply { reverse() }
+    println("Arreglo en orden inverso:")
+    for (elemento in arregloInverso) {
+        println(elemento)
+    }
+}
+
+fun mostrarArregloNormal(arreglo: IntArray) {
+    println("Arreglo en orden normal:")
+    for (elemento in arreglo) {
+        println(elemento)
+    }
+}
+
+fun mostrarSumaArreglo(arreglo: IntArray) {
+    var suma = 0
+    println("Suma del arreglo:")
+    for (elemento in arreglo) {
+        suma += elemento
+    }
+    println(suma)
+}
+
+fun mostrarPrimerYUltimoElemento(arreglo: IntArray) {
+    println("El primer elemento del arreglo es: ${arreglo[0]}")
+    println("El último elemento del arreglo es: ${arreglo[arreglo.size - 1]}")
+}
+
+fun agregarUnNuevoElementoYSumarlo(arreglo: IntArray) {
+    var nuevoElemento: Int? = null
+
+    do {
+        println("Ingrese un nuevo elemento para sumar a los existentes en el arreglo:")
+        try {
+            nuevoElemento = readLine()?.toInt()
+            if (nuevoElemento == null) throw Exception()
+        } catch (e: Exception) {
+            println("Ingrese un número válido.")
+        }
+    } while (nuevoElemento == null)
+
+    val nuevoArreglo = arreglo.plus(nuevoElemento)
+    var suma = 0
+    for (elemento in nuevoArreglo) {
+        suma += elemento
+    }
+
+    println("La nueva suma del arreglo es: $suma")
+}
+
+
