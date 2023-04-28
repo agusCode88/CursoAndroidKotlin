ffbcd724f73e09f7745a981e54fea6024b7c8d2e
UrzuaAlarcon <122647461+UrzuaAlarcon@users.noreply.github.com>
2023-04-28 19:16:04 -0400

    tratando de salvar la cosa 2

--- tareas/TareaMapas.kt
+++ tareas/TareaMapas.kt
@@ -1,1 +1,65 @@
-package com.example.aprend
+package com.example.aprendiendokotlin.tareas
+
+fun main() {
+    val estudiante = mutableMapOf<String, Int>() // Diccionario de Esudiante (nombre y edad)
+
+
+    do {
+        println("╔══════════════════════════════════╗")
+        println("║       MENÚ ESTUDIANTES ANDROID   ║")
+        println("╠══════════════════════════════════╣")
+        println("║ 1. Agregar estudiante            ║")
+        println("║ 2. Mostrar lista de estudiantes  ║")
+        println("║ 3. Eliminar estudiante           ║")
+
+        println("║ 6. Total de estudiantes          ║ ")
+        println("║ 7. Salir                         ║")
+        println("╚══════════════════════════════════╝")
+
+        print("Ingresa tu opción: ")
+        val opcion = readLine()?.toIntOrNull() ?: 0
+
+        when (opcion) {
+            1 -> {
+                print("Ingresa el nombre de la persona: ")
+                val nombre = readLine()?.toString() ?: ""
+                print("Ingresa la edad de la persona: ")
+                val edad = readLine()?.toInt() ?: 0
+                estudiante[nombre] = edad
+                println("La persona $nombre ha sido agregada correctamente.")
+            }
+            2 -> {
+                if (estudiante.isEmpty()) {
+                    println("No hay personas en la lista.")
+                } else {
+                    println("Lista de personas:")
+                    estudiante.forEach { (name, age) ->
+                        println("- $name ($age años)")
+                    }
+                }
+            }
+            3 -> {
+                print("Ingresa el nombre de la persona que deseas eliminar: ")
+                val nombre = readLine()?.toString() ?: ""
+                if (estudiante.containsKey(nombre)) {
+                    estudiante.remove(nombre)
+                    println("La persona $nombre ha sido eliminada correctamente.")
+                } else {
+                    println("La persona $nombre no se encontró en la lista.")
+                }
+            }
+            6-> {
+                mostrarDeCantidadPersonas(estudiante)}
+            7 -> {
+                println("Saliendo del programa...")
+            }
+            else -> {
+                println("Opción inválida. Inténtalo de  nuevo.")
+            }
+        }
+    } while (opcion != 7)
+}
+fun mostrarDeCantidadPersonas(estudiante: MutableMap<String, Int>) {
+    val cantidadDePersonas = estudiante.size
+    println("Hay $cantidadDePersonas estudiantes Android.")
+}
