d7c073ac6af89fe3aa51cfc46df47aa460302f11
StivenFonseca <stiven.jfonseca@gmail.com>
2023-04-27 22:02:21 -0400

    h

--- Tarea mapas.kt
+++ Tarea mapas.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.example.aprendiendokotlin
 
 /*
@@ -6,14 +8,14 @@ Necesario: Llenar la lista de estudiantes
 
 Ideas:
 
-*Ordenar la lista de personas por nombre o por edad.
-*Permitir que el usuario busque una persona por nombre o por edad.
-*Guardar la lista de personas en un archivo y cargarla al iniciar el programa.
-*Agregar validación de entrada de usuario para evitar errores (por ejemplo, asegurarse de que la edad ingresada sea un número entero válido).
-*Agregar una función para mostrar la persona más joven y la persona más vieja en la lista.
-*Permitir al usuario actualizar la edad de una persona existente.
-*Agregar una función para mostrar la cantidad total de personas en la lista.
-*Agregar la posibilidad de agregar más información sobre la persona, como su dirección o su número de teléfono.
+*1.Ordenar la lista de personas por nombre o por edad.
+*2.Permitir que el usuario busque una persona por nombre o por edad.
+*3.Guardar la lista de personas en un archivo y cargarla al iniciar el programa.
+*4.Agregar validación de entrada de usuario para evitar errores (por ejemplo, asegurarse de que la edad ingresada sea un número entero válido).
+*5.Agregar una función para mostrar la persona más joven y la persona más vieja en la lista.
+*6.Permitir al usuario actualizar la edad de una persona existente.
+*7.Agregar una función para mostrar la cantidad total de personas en la lista.
+*8.Agregar la posibilidad de agregar más información sobre la persona, como su dirección o su número de teléfono.
 
  */
 
@@ -29,6 +31,7 @@ fun main() {
         println("║ 1. Agregar estudiante            ║")
         println("║ 2. Mostrar lista de estudiantes  ║")
         println("║ 3. Eliminar estudiante           ║")
+        println("║ 6. actualizar la edad            ║")
         println("║ 4. Salir                         ║")
         println("╚══════════════════════════════════╝")
 
@@ -71,5 +74,18 @@ fun main() {
                 println("Opción inválida. Inténtalo de nuevo.")
             }
         }
+
+        5 -> {
+            print("Ingresa el nombre de la persona cuya edad deseas actualizar: ")
+            val nombre = readLine()?.toString() ?: ""
+            if (estudiante.containsKey(nombre)) {
+                print("Ingresa la nueva edad de la persona: ")
+                val edad = readLine()?.toInt() ?: 0
+                estudiante[nombre] = edad
+                println("La edad de la persona $nombre ha sido actualizada correctamente.")
+            } else {
+                println("La persona $nombre no se encontró en la lista.")
+
     } while (opcion != 4)
-}
+
+}}
