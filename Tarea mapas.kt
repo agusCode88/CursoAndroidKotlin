6d1ba8d1ed909fa96e2ed448b1590812295fd9d8
Moises Segura A <moises.segura94@gmail.com>
2023-04-28 13:06:06 -0400

    Menu + Mostrar mas joven y mas viejo

--- Tarea mapas.kt
+++ Tarea mapas.kt
@@ -19,86 +19,293 @@ Ideas:
 
 
 fun main() {
-
-    val estudiante = mutableMapOf<String, Int>() // Diccionario de Esudiante (nombre y edad)
+    val estudiantes = mutableMapOf<String, Int>() // Diccionario de Estudiantes (nombre y edad)
     var edadMaxima = 0
     var edadMinima = Int.MAX_VALUE
     var nombrePersonaMasJoven: String? = null
     var nombrePersonaMasVieja: String? = null
 
+
     do {
-        println("╔════════════════════════════════════════════════╗")
-        println("║       MENÚ ESTUDIANTES ANDROID                 ║")
-        println("╠════════════════════════════════════════════════╣")
-        println("║ 1. Agregar estudiante                          ║")
-        println("║ 2. Mostrar lista de estudiantes                ║")
-        println("║ 3. Eliminar estudiante                         ║")
-        println("║ 4. Ordenar del mas joven al mas viejo          ║")
-        println("║ 5. Mostrar cantidad de personas                ║")
-        println("║ 6. Salir                                       ║")
-        println("╚════════════════════════════════════════════════╝")
+        println("╔════════════════════════════════════╗")
+        println("║ MENÚ PRINCIPAL ESTUDIANTES ANDROID ║")
+        println("╠════════════════════════════════════╣")
+        println("║ 1. Agregar estudiante              ║")
+        println("║ 2. Mostrar lista de estudiantes    ║") //*Agregar una función para mostrar la cantidad total de personas en la lista.
+        println("║ 3. Buscar estudiante               ║") //Permitir que el usuario busque una persona por nombre o por edad.
+        println("║ 4. Ordenar lista de estudiantes    ║") //*Ordenar la lista de personas por nombre o por edad.
+        println("║ 5. Actualizar estudiantes          ║") // *Agregar la posibilidad de agregar más información sobre la persona, como su dirección o su número de teléfono.
+        println("║ 6. Ver archivo de estudiantes      ║") //*Guardar la lista de personas en un archivo y cargarla al iniciar el programa.
+        println("║ 7. Salir                           ║")
+        println("╚════════════════════════════════════╝")
 
         print("Ingresa tu opción: ")
-        val opcion = readLine()?.toInt() ?: 0
-
-        when (opcion) {
-            1 -> {
-                print("Ingresa el nombre de la persona: ")
-                val nombre = readLine()?.toString() ?: ""
-                print("Ingresa la edad de la persona: ")
-                val edad = readLine()?.toInt() ?: 0
-                estudiante[nombre] = edad
-                println("La persona $nombre ha sido agregada correctamente.")
-            }
-            2 -> {
-                if (estudiante.isEmpty()) {
-                    println("No hay personas en la lista.")
-                } else {
-                    println("Lista de personas:")
-                    estudiante.forEach { (name, age) ->
-                        println("- $name ($age años)")
-                    }
-                }
-            }
-            3 -> {
-                print("Ingresa el nombre de la persona que deseas eliminar: ")
-                val nombre = readLine()?.toString() ?: ""
-                if (estudiante.containsKey(nombre)) {
-                    estudiante.remove(nombre)
-                    println("La persona $nombre ha sido eliminada correctamente.")
-                } else {
-                    println("La persona $nombre no se encontró en la lista.")
-                }
-            }
-            4 -> {
-                if (estudiante.isEmpty()) {
-                    println("No hay personas en la lista.")
-                } else {
-                    println("Lista de personas:")
-                    estudiante.forEach { (name, age) ->
-                        println("- $name ($age años)")
-                        if (age < edadMinima) {
-                            edadMinima = age
-                            nombrePersonaMasJoven = name
-                        }
-                        if (age > edadMaxima) {
-                            edadMaxima = age
-                            nombrePersonaMasVieja = name
-                        }
-                    }
-                    println("La persona más joven es $nombrePersonaMasJoven, con $edadMinima años.")
-                    println("La persona más vieja es $nombrePersonaMasVieja, con $edadMaxima años.")
-                }
-            }
-            5 -> {
-                println("La cantidad de personas en la lista es: ${estudiante.size}")
-            }
-            6 -> {
-                println("Saliendo del programa...")
-            }
-            else -> {
+        var opcionMenuPrincipal: Int? = null
+        do {
+            opcionMenuPrincipal = readLine()?.toIntOrNull()
+            if (opcionMenuPrincipal == null || opcionMenuPrincipal !in 1..7) {
                 println("Opción inválida. Inténtalo de nuevo.")
             }
+        } while (opcionMenuPrincipal == null || opcionMenuPrincipal !in 1..7)
+
+
+        when (opcionMenuPrincipal) {
+            1 -> {
+                print("Ingresa el nombre del estudiante: ")
+                val nombre = readLine()?.toString() ?: ""
+                print("Ingresa la edad del estudiante: ")
+                val edad = readLine()?.toInt() ?: 0
+                estudiantes[nombre] = edad
+                println("El estudiante $nombre ha sido agregado correctamente.")
+            }
+
+            2 -> {
+                if (estudiantes.isEmpty()) {
+                    println("No hay personas en la lista.")
+                } else {
+                    println("Lista de personas (${estudiantes.size} personas en total):")
+                    estudiantes.forEach { (nombre, edad) ->
+                        println("- $nombre ($edad años)")
+                    }
+                }
+            }
+
+            3 -> do {
+                println("╔══════════════════════════════════════════════╗")
+                println("║ MENÚ BUSCAR ESTUDIANTES ANDROID              ║")
+                println("╠══════════════════════════════════════════════╣")
+                println("║ 1. Buscar un estudiante por nombre           ║")
+                println("║ 2. Buscar un estudiante por edad             ║")
+                println("║ 3. Volver al menú principal                  ║")
+                println("║ 4. Salir                                     ║")
+                println("╚══════════════════════════════════════════════╝")
+
+                print("Ingresa tu opción: ")
+                var opcionMenuBuscar: Int? = null
+                do {
+                    opcionMenuBuscar = readLine()?.toIntOrNull()
+                    if (opcionMenuBuscar == null || opcionMenuBuscar !in 1..4) {
+                        println("Opción inválida. Inténtalo de nuevo.")
+                    }
+                } while (opcionMenuBuscar == null || opcionMenuBuscar !in 1..4)
+
+                when (opcionMenuBuscar) {
+                    1 -> {
+                        print("Ingresa el nombre del estudiante que deseas buscar: ")
+                        val nombreABuscar = readLine()?.toString() ?: ""
+                        val edad = estudiantes[nombreABuscar]
+                        if (edad != null) {
+                            println("El estudiante $nombreABuscar tiene $edad años.")
+                        } else {
+                            println("No se encontró ningún estudiante con el nombre $nombreABuscar.")
+                        }
+                    }
+
+                    2 -> {
+                        print("Ingresa la edad del estudiante que deseas buscar: ")
+                        val edadABuscar = readLine()?.toInt() ?: 0
+                        val estudiantesConEdad = estudiantes.filterValues { it == edadABuscar }
+                        if (estudiantesConEdad.isNotEmpty()) {
+                            println("Los estudiantes con $edadABuscar años son:")
+                            estudiantesConEdad.keys.forEach { println("- $it") }
+                        } else {
+                            println("No se encontró ningún estudiante con $edadABuscar años.")
+                        }
+                    }
+
+                    3 -> break
+
+                    4 -> return
+
+                    else -> {
+                        println("Opción inválida.")
+                    }
+                }
+                println("Presiona ENTER para volver al menú buscar.")
+                readLine()
+            } while (true)
+
+            4 -> do {
+
+                println("╔══════════════════════════════════════════════╗")
+                println("║      MENÚ ORDENAR ESTUDIANTES ANDROID        ║")
+                println("╠══════════════════════════════════════════════╣")
+                println("║ 1. Ordenar la lista de personas por nombre   ║") //*Ordenar la lista de personas por nombre o por edad.
+                println("║ 2. Ordenar la lista de personas por edad     ║") //*Ordenar la lista de personas por nombre o por edad.
+                println("║ 3. Mostrar la mas joven y la mas vieja       ║") //*Agregar una función para mostrar la persona más joven y la persona más vieja en la lista.
+                println("║ 4. Volver al menú principal                  ║") //Volver al menú
+                println("║ 5. Salir                                     ║") //Salir
+                println("╚══════════════════════════════════════════════╝")
+
+                print("Ingresa tu opción: ")
+                var opcionMenuOrdenar: Int? = null
+                do {
+                    opcionMenuOrdenar = readLine()?.toIntOrNull()
+                    if (opcionMenuOrdenar == null || opcionMenuOrdenar !in 1..6) {
+                        println("Opción inválida. Inténtalo de nuevo.")
+                    }
+                } while (opcionMenuOrdenar == null || opcionMenuOrdenar !in 1..6)
+
+
+                when (opcionMenuOrdenar) {
+                    1 -> {
+                        println("Aqui va la funcion de ordenar por nombre")
+
+                    }
+
+                    2 -> {
+                        println("Aqui va la funcion de ordenar por edad")
+
+                    }
+
+                    3 -> {
+                        if (estudiantes.isEmpty()) {
+                            println("No hay personas en la lista.")
+                        } else {
+                            println("Lista de personas:")
+                            estudiantes.forEach { (name, age) ->
+                                println("- $name ($age años)")
+                                if (age < edadMinima) {
+                                    edadMinima = age
+                                    nombrePersonaMasJoven = name
+                                }
+                                if (age > edadMaxima) {
+                                    edadMaxima = age
+                                    nombrePersonaMasVieja = name
+                                }
+                            }
+                            println("La persona más joven es $nombrePersonaMasJoven, con $edadMinima años.")
+                            println("La persona más vieja es $nombrePersonaMasVieja, con $edadMaxima años.")
+                        }
+
+                    }
+                    4 -> break
+
+                    5 -> return
+
+                    else -> {
+                        println("Opción inválida.")
+                    }
+                }
+                println("Presiona ENTER para volver al menú ordenar.")
+                readLine()
+            } while (true)
+
+            5 -> do {
+                println("╔══════════════════════════════════════════════╗")
+                println("║     MENÚ DE ACTUALIZACIÓN LISTA ESTUDIANTES  ║")
+                println("╠══════════════════════════════════════════════╣")
+                println("║ 1. Actualizar la edad del estudiante         ║") // *Permitir al usuario actualizar la edad de una persona existente.
+                println("║ 2. Agregar dirección del estudiante          ║") //*Agregar la posibilidad de agregar más información sobre la persona, como su dirección o su número de teléfono.
+                println("║ 3. Agregar teléfono  del estudiante          ║") //*Agregar la posibilidad de agregar más información sobre la persona, como su dirección o su número de teléfono.
+                println("║ 4. Agregar información adicional             ║") //*Agregar la posibilidad de agregar más información sobre la persona, como su dirección o su número de teléfono.
+                println("║ 5. Eliminar estudiante                       ║") //*Permitir al usuario eliminar una persona existente.
+                println("║ 6. Volver al menú principal                  ║") //Volver al menú
+                println("║ 7. Salir                                     ║") //Salir
+                println("╚══════════════════════════════════════════════╝")
+
+                print("Ingresa tu opción: ")
+                var opcionMenuActualizar: Int? = null
+                do {
+                    opcionMenuActualizar = readLine()?.toIntOrNull()
+                    if (opcionMenuActualizar == null || opcionMenuActualizar !in 1..7) {
+                        println("Opción inválida. Inténtalo de nuevo.")
+                    }
+                } while (opcionMenuActualizar == null || opcionMenuActualizar !in 1..7)
+
+                when (opcionMenuActualizar) {
+                    1 -> {
+                        println("Aquí va la funcion actualizar edad")
+
+                    }
+
+                    2 -> {
+                        println("Aquí va la funcion agregar dirección")
+
+                    }
+
+                    3 -> {
+                        println("Aquí va la funcion agregar teléfono")
+
+                    }
+
+                    4 -> {
+                        println("Aquí va la funcion agregar información adicional")
+                    }
+
+                    5 -> {
+                        print("Ingresa el nombre de la persona que deseas eliminar: ")
+                        val nombre = readLine()?.toString() ?: ""
+                        if (estudiantes.containsKey(nombre)) {
+                            estudiantes.remove(nombre)
+                            println("La persona $nombre ha sido eliminada correctamente.")
+                        } else {
+                            println("La persona $nombre no se encontró en la lista.")
+                        }
+                    }
+
+                    6 -> break
+
+                    7 -> return
+
+                    else -> {
+                        println("Opción inválida.")
+                    }
+                }
+                println("Presiona ENTER para volver al menú actualizar.")
+                readLine()
+            } while (true)
+
+            6 -> do {
+
+                println("╔══════════════════════════════════════════════╗")
+                println("║      MENÚ ARCHIVO ESTUDIANTES ANDROID        ║")
+                println("╠══════════════════════════════════════════════╣")
+                println("║ 1. Guardar la lista de personas en un archivo║") //*Guardar la lista de personas en un archivo y cargarla al iniciar el programa.
+                println("║ 2. Cargar la lista de personas desde archivo ║") //*Guardar la lista de personas en un archivo y cargarla al iniciar el programa.
+                println("║ 3. Validar entrada de usuario                ║") //*Agregar validación de entrada de usuario para evitar errores (por ejemplo, asegurarse de que la edad ingresada sea un número entero válido).
+                println("║ 4. Volver al menú principal                  ║") //Volver al menú
+                println("║ 5. Salir                                     ║") //Salir
+                println("╚══════════════════════════════════════════════╝")
+
+                print("Ingresa tu opción: ")
+                var opcionMenuArchivo: Int? = null
+                do {
+                    opcionMenuArchivo = readLine()?.toIntOrNull()
+                    if (opcionMenuArchivo == null || opcionMenuArchivo !in 1..5) {
+                        println("Opción inválida. Inténtalo de nuevo.")
+                    }
+                } while (opcionMenuArchivo == null || opcionMenuArchivo !in 1..5)
+
+
+                when (opcionMenuArchivo) {
+                    1 -> {
+                        println("Aqui va la funcion de guardar")
+
+                    }
+
+                    2 -> {
+                        println("Aqui va la funcion de cargar")
+
+                    }
+
+                    3 -> {
+                        println("Aqui va la funcion de validar")
+
+                    }
+
+                    4 -> break
+
+                    5 -> return
+
+                    else -> {
+                        println("Opción inválida.")
+                    }
+                }
+                println("Presiona ENTER para volver al menú archivo.")
+                readLine()
+            } while (true)
+
         }
-    } while (opcion != 4)
+    } while (opcionMenuPrincipal != 7)
 }
