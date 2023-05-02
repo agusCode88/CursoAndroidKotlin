97334aaa4194eb79cdfcb39dfe6e485d68381c64
Moises Segura A <moises.segura94@gmail.com>
2023-05-02 18:36:00 -0400

    Clases kotlin

---
+++ POO/Mensajeria.kt
@@ -0,0 +1,11 @@
+package com.example.aprendiendokotlin.POO
+
+class Mensajeria (estudiante : Estudiante){
+
+
+        var estudianteAgregado = "El estudiante ha sido agregado"
+        var estudianteNoAgregado = "El estudiante no ha sido agregado"
+
+
+    constructor():this(Estudiante())
+}
---
+++ POO/Relator.kt
@@ -0,0 +1,10 @@
+package com.example.aprendiendokotlin.POO
+
+class Relator(nombre:String, sueldo:Int) {
+
+    var nombre:String = nombre
+    var sueldo:Int = sueldo
+
+    constructor():this("", 0)
+
+}
