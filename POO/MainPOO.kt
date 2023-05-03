5984819cec8db5bf090fa7efebd34d17a2b1fea4
Moises Segura A <moises.segura94@gmail.com>
2023-05-03 19:25:17 -0400

    Rama grupo

--- POO/MainPOO.kt
+++ POO/MainPOO.kt
@@ -2,23 +2,4 @@ package com.example.aprendiendokotlin.POO
 
 
 fun main() {
-
-    var curso = CursoAndroid("Curso de Android", Relator())
-
-    var moises = Estudiante("18.593.077-7", "Moises", 28, "08/08/1994")
-    var miguel = Estudiante("11.111.111-1", "Miguel", 28, "08/08/1994")
-    var sergio = Estudiante("18.593.077-7", "Sergio", 28, "08/08/1994")
-
-
-    moises.estudianteSePresenta()
-    moises.estudianteAsiste()
-    moises.estudianteHabla()
-
-
-    miguel.estudianteSePresenta()
-    miguel.estudianteCome()
-    miguel.estudianteEstudia()
-
-
-    curso.agregarEstudiante(sergio)
 }
